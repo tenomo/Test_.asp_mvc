3666db98eb88f580c24534c04ba24aa392fc683a
Tenomo <tenomo22@gmail.com>
2016-04-06 03:07:14 +0300

    last commit

---
+++ WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {      
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        { 
+
+            return View();
+        }
+
+
+         [HttpGet]
+        public ActionResult UserSubscriber()
+        {
+
+            return View();
+        }
+
+     public static   Models.UsersDate userData = new Models.UsersDate();
+
+        // [HttpPost]
+        //public ActionResult UserSubscriber(string firstName, string lastName, string email)
+        //{
+        //    ViewBag.Message = "input strings";
+        //    userData.AddUser(firstName, lastName, email);
+        //    return View();
+        //}
+
+        [HttpPost]
+        public  ActionResult  UserSubscriber(Models.User user)
+        {
+
+            try {
+                ViewBag.Message = "input user";
+                userData.AddUser(user);
+            }
+            catch
+            {
+
+            }
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult ShowUsers()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication/Models/UsersDate.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    /// <summary>
+    /// Emulation data base users
+    /// </summary>
+    public class UsersDate
+    {
+       private  List<User> _Users = new List<User>();
+
+        public List<User> Users
+        {
+            get
+            {
+                return _Users;
+            }
+        }
+
+        public void AddUser(string FirstName, string LastName, string EmailAddres )
+        {
+            Users.Add( new User(FirstName, LastName, EmailAddres));
+        }
+        public void AddUser(User user)
+        {
+            Users.Add(user);
+        }
+
+    }
+
+     
+    public class User
+    {
+          public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddres { get; set; } 
+
+
+
+        public User (string FirstName, string LastName, string EmailAddres)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.EmailAddres = EmailAddres;
+        }
+
+
+        public User( )
+        { 
+        }
+
+
+        public override string ToString()
+        {
+            return FirstName + " " + LastName + " " + EmailAddres;
+        }
+
+        // other
+        // ...
+        //
+
+    }
+}
